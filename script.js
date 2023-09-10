4e95a2665247e69a178ad1998d47ae58f9ce133c
bellasigur <144618915+bellasigur@users.noreply.github.com>
2023-09-10 23:49:18 +0000

    Version 1.0

--- script.js
+++ script.js
@@ -2,30 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
     const steps = document.querySelectorAll(".step");
     const nextButtons = document.querySelectorAll(".next-button");
     const prevButtons = document.querySelectorAll(".prev-button");
-    const calculatorForm = document.querySelector("form");
-
     let currentStep = 0;
 
-    // Function to show a specific step
+    const startButton = document.getElementById("startButton");
+    startButton.addEventListener("click", function () {
+        document.getElementById('frontpage').style.display = "none";
+        document.getElementById('about').style.display = "none";
+        document.getElementById('content').style.display = "block";
+    });
+
     function showStep(stepNumber) {
         steps.forEach((step, index) => {
-            if (index === stepNumber) {
-                step.style.display = "block";
-            } else {
-                step.style.display = "none";
-            }
+            step.style.display = index === stepNumber ? "block" : "none";
         });
     }
 
-    // Function to navigate to the next step
     function goToNextStep() {
         if (currentStep < steps.length - 1) {
+            if (currentStep + 1 === steps.length - 1) {
+                const totalFootprint = calculateCarbonFootprint();
+                displayResult(totalFootprint);
+            }
             currentStep++;
             showStep(currentStep);
         }
     }
 
-    // Function to navigate to the previous step
     function goToPrevStep() {
         if (currentStep > 0) {
             currentStep--;
@@ -33,51 +35,136 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Event listeners for the "Next" buttons
     nextButtons.forEach((button) => {
         button.addEventListener("click", goToNextStep);
     });
 
-    // Event listeners for the "Previous" buttons
     prevButtons.forEach((button) => {
         button.addEventListener("click", goToPrevStep);
     });
 
-    // Function to calculate and display the estimated carbon footprint
     function calculateCarbonFootprint() {
         const travelMethod = parseFloat(document.getElementById("travelMethod").value);
         const meatConsumption = parseFloat(document.getElementById("meatConsumption").value);
-
-        // Calculate total carbon footprint based on selected options
-        const totalFootprint = travelMethod + meatConsumption;
-
-        return totalFootprint;
+        const homeConsumption = parseFloat(document.getElementById("homestats").value);
+        const serviceConsumption = parseFloat(document.getElementById("serviceConsumption").value);
+        const goodsConsumption = parseFloat(document.getElementById("goodsConsumption").value);
+        return travelMethod + meatConsumption + homeConsumption + serviceConsumption + goodsConsumption;
     }
 
-    // Event listener for form submission
-    calculatorForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-
-        // Calculate the carbon footprint
-        const totalFootprint = calculateCarbonFootprint();
-
-        // Display the result in the "Result" step
-        displayResult(totalFootprint);
-
-        // Move to the "Result" step
-        currentStep = steps.length - 1;
-        showStep(currentStep);
-    });
-
-    // Function to display the calculated result
     function displayResult(result) {
         const resultElement = document.getElementById("result");
-        resultElement.innerHTML = `
-            <p>Your estimated carbon footprint is: <strong>${result.toFixed(2)} tons CO2 per year</strong></p>
-            <p>Thank you for calculating your carbon footprint!</p>
-        `;
+        const data = JSON.parse(localStorage.getItem('i18n'));
+        resultElement.innerHTML = `${data.resultText.replace('{result}', result.toFixed(2))} <br> ${data.thanksMsg}`;
     }
 
-    // Show the initial step
+    function i18nUpdate() {
+        const data = JSON.parse(localStorage.getItem('i18n'));
+
+        // Populate titles and other text elements
+        document.getElementById('title').textContent = data.title;
+        document.getElementById('main-title').textContent = data.mainTitle;
+        document.getElementById('step1-title').textContent = data.step1Title;
+        document.getElementById('step2-title').textContent = data.step2Title;
+        document.getElementById('step3-title').textContent = data.step3Title;
+        document.getElementById('step4-title').textContent = data.step4Title;
+        document.getElementById('step5-title').textContent = data.step5Title;
+        document.getElementById('step6-title').textContent = data.step6Title;
+        document.getElementById('travel-label').textContent = data.travelLabel;
+        document.getElementById('welcomeTitle').textContent = data.welcomeTitle;
+        document.getElementById('welcomeMessage').textContent = data.welcomeMessage;
+        document.getElementById('aboutTitle').textContent = data.aboutTitle;
+        document.getElementById('aboutMessage').textContent = data.aboutMessage;
+        document.getElementById('startButton').textContent = data.startButton;
+
+        // Populate travelMethod options
+        const travelMethodSelect = document.getElementById("travelMethod");
+        travelMethodSelect.innerHTML = "";
+        for (const key in data.travelOptions) {
+            const option = document.createElement("option");
+            option.text = data.travelOptions[key].text;
+            option.value = data.travelOptions[key].value;
+            travelMethodSelect.add(option);
+        }
+
+        // Populate meatConsumption options
+        const meatConsumptionSelect = document.getElementById("meatConsumption");
+        meatConsumptionSelect.innerHTML = "";
+        for (const key in data.meatOptions) {
+            const option = document.createElement("option");
+            option.text = data.meatOptions[key].text;
+            option.value = data.meatOptions[key].value;
+            meatConsumptionSelect.add(option);
+        }
+
+        // Populate homestats options
+        const homeSelect = document.getElementById("homestats");
+        homeSelect.innerHTML = "";
+        for (const key in data.homeOptions) {
+            const option = document.createElement("option");
+            option.text = data.homeOptions[key].text;
+            option.value = data.homeOptions[key].value;
+            homeSelect.add(option);
+        }
+
+        // Populate serviceConsumption options
+        const serviceConsumptionSelect = document.getElementById("serviceConsumption");
+        serviceConsumptionSelect.innerHTML = "";
+        for (const key in data.serviceConsumptionOptions) {
+            const option = document.createElement("option");
+            option.text = data.serviceConsumptionOptions[key].text;
+            option.value = data.serviceConsumptionOptions[key].value;
+            serviceConsumptionSelect.add(option);
+        }
+
+        // Populate goodsConsumption options
+        const goodsConsumptionSelect = document.getElementById("goodsConsumption");
+        goodsConsumptionSelect.innerHTML = "";
+        for (const key in data.goodsConsumptionOptions) {
+            const option = document.createElement("option");
+            option.text = data.goodsConsumptionOptions[key].text;
+            option.value = data.goodsConsumptionOptions[key].value;
+            goodsConsumptionSelect.add(option);
+        }
+
+        document.getElementById('travel-label').textContent = data.travelLabel;
+        document.getElementById('meat-label').textContent = data.meatLabel;
+        document.getElementById('homestats-label').textContent = data.homeLabel;
+        document.getElementById('serviceConsumption-label').textContent = data.serviceConsumptionLabel;
+        document.getElementById('goodsConsumption-label').textContent = data.goodsConsumptionLabel;
+
+        nextButtons.forEach((button) => {
+            button.textContent = data.nextButtonText;
+        });
+
+        prevButtons.forEach((button) => {
+            button.textContent = data.prevButtonText;
+        });
+
+    }
+
     showStep(currentStep);
+
+    document.getElementById('lang').addEventListener('change', function () {
+        const lang = this.value;
+        fetch(lang + '.json')
+            .then(response => response.json())
+            .then(data => {
+                localStorage.setItem('i18n', JSON.stringify(data));
+                i18nUpdate();
+            });
+    });
+
+    const userLang = navigator.language || navigator.userLanguage;
+    const langDropDown = document.getElementById('lang');
+
+    if (userLang.includes('is')) {
+        langDropDown.value = 'is';
+    } else if (userLang.includes('da')) {
+        langDropDown.value = 'da';
+    } else {
+        langDropDown.value = 'en';
+    }
+
+    langDropDown.dispatchEvent(new Event('change'));
 });
