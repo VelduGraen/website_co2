7cfff910f1cb171579ee4f0fe68cd546a50692aa
bellasigur <144618915+bellasigur@users.noreply.github.com>
2023-09-10 17:42:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
     const calculatorForm = document.getElementById("calculatorForm");
-    const calculateButton = document.getElementById("calculateButton");
+    const steps = document.querySelectorAll(".step");
+    const prevButtons = document.querySelectorAll(".prev-button");
+    const nextButtons = document.querySelectorAll(".next-button");
     const carbonFootprintSpan = document.getElementById("carbonFootprint");
 
+    let currentStep = 0;
+
+    // Function to navigate to the next step
+    function goToNextStep() {
+        if (currentStep < steps.length - 1) {
+            steps[currentStep].style.display = "none";
+            currentStep++;
+            steps[currentStep].style.display = "block";
+        }
+    }
+
+    // Function to navigate to the previous step
+    function goToPrevStep() {
+        if (currentStep > 0) {
+            steps[currentStep].style.display = "none";
+            currentStep--;
+            steps[currentStep].style.display = "block";
+        }
+    }
+
+    // Event listeners for next and previous buttons
+    nextButtons.forEach((button) => {
+        button.addEventListener("click", goToNextStep);
+    });
+
+    prevButtons.forEach((button) => {
+        button.addEventListener("click", goToPrevStep);
+    });
+
     // Function to calculate carbon footprint
-    function calculateCarbonFootprint() {
-        // Step 1: Travel
+    function calculateCarbonFootprint(e) {
+        e.preventDefault();
+
+        // Collect and calculate the data from each step here
         const carMiles = parseFloat(document.getElementById("carMiles").value);
-        // Add more travel-related variables here
-
-        // Step 2: Food
         const meatConsumption = parseFloat(document.getElementById("meatConsumption").value);
-        // Add more food-related variables here
-
-        // Step 3: Home
         const electricityUsage = parseFloat(document.getElementById("electricityUsage").value);
-        // Add more home-related variables here
-
-        // Step 4: Consumption
         const clothingSpending = parseFloat(document.getElementById("clothingSpending").value);
-        // Add more consumption-related variables here
 
         // Calculate total carbon footprint (a simple sum for demonstration purposes)
         const totalFootprint = carMiles + meatConsumption * 10 + electricityUsage * 0.5 + clothingSpending * 0.02;
@@ -27,5 +50,5 @@ document.addEventListener("DOMContentLoaded", function () {
         carbonFootprintSpan.textContent = totalFootprint.toFixed(2) + " kg CO2e/year";
     }
 
-    calculateButton.addEventListener("click", calculateCarbonFootprint);
+    calculatorForm.addEventListener("submit", calculateCarbonFootprint);
 });
