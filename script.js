0369a00c8ef87e226ac7669c16e2bfefcf117de0
bellasigur <144618915+bellasigur@users.noreply.github.com>
2023-09-10 19:42:31 +0000

    Update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const steps = document.querySelectorAll(".step");
-    const prevButtons = document.querySelectorAll(".prev-button");
     const nextButtons = document.querySelectorAll(".next-button");
+    const prevButtons = document.querySelectorAll(".prev-button");
     const calculatorForm = document.querySelector("form");
 
     let currentStep = 0;
 
+    // Function to show a specific step
     function showStep(stepNumber) {
         steps.forEach((step, index) => {
             if (index === stepNumber) {
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    // Function to navigate to the next step
     function goToNextStep() {
         if (currentStep < steps.length - 1) {
             currentStep++;
@@ -23,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    // Function to navigate to the previous step
     function goToPrevStep() {
         if (currentStep > 0) {
             currentStep--;
@@ -30,36 +33,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    // Event listeners for next and previous buttons
-    nextButtons.forEach((button, index) => {
-        button.addEventListener("click", () => {
-            goToNextStep();
-        });
+    // Event listeners for the "Next" buttons
+    nextButtons.forEach((button) => {
+        button.addEventListener("click", goToNextStep);
     });
 
-    prevButtons.forEach((button, index) => {
-        button.addEventListener("click", () => {
-            goToPrevStep();
-        });
+    // Event listeners for the "Previous" buttons
+    prevButtons.forEach((button) => {
+        button.addEventListener("click", goToPrevStep);
     });
 
     // Function to calculate and display the estimated carbon footprint
     function calculateCarbonFootprint() {
-        const carMiles = parseFloat(document.getElementById("carMiles").value);
+        const travelMethod = parseFloat(document.getElementById("travelMethod").value);
         const meatConsumption = parseFloat(document.getElementById("meatConsumption").value);
-        const electricityUsage = parseFloat(document.getElementById("electricityUsage").value);
-        const clothingSpending = parseFloat(document.getElementById("clothingSpending").value);
 
-        // Calculate total carbon footprint based on some arbitrary factors for demonstration purposes
-        const carEmissionFactor = 2.5; // kg CO2e per mile
-        const meatEmissionFactor = 20; // kg CO2e per kg of meat
-        const electricityEmissionFactor = 0.5; // kg CO2e per kWh
-        const clothingEmissionFactor = 0.02; // kg CO2e per dollar spent on clothing
-
-        const totalFootprint = (carMiles * carEmissionFactor) +
-                              (meatConsumption * meatEmissionFactor) +
-                              (electricityUsage * electricityEmissionFactor) +
-                              (clothingSpending * clothingEmissionFactor);
+        // Calculate total carbon footprint based on selected options
+        const totalFootprint = travelMethod + meatConsumption;
 
         return totalFootprint;
     }
@@ -83,7 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function displayResult(result) {
         const resultElement = document.getElementById("result");
         resultElement.innerHTML = `
-            <p>Your estimated carbon footprint is: <strong>${result.toFixed(2)} kg CO2e/year</strong></p>
+            <p>Your estimated carbon footprint is: <strong>${result.toFixed(2)} tons CO2 per year</strong></p>
             <p>Thank you for calculating your carbon footprint!</p>
         `;
     }
